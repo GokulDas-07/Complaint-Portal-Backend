b0f13d2a77cb194088b195c47e97d6826084cd94
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-09 15:11:58 +0530

    user files added

---
+++ src/main/java/com/nest/complaintportal_backend/model/User.java
@@ -0,0 +1,90 @@
+package com.nest.complaintportal_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+    private String address;
+    private int phone;
+    private String mail;
+    private String password;
+    private String confirm;
+
+    public User() {
+    }
+
+    public User(int id, String name, String address, int phone, String mail, String password, String confirm) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.mail = mail;
+        this.password = password;
+        this.confirm = confirm;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirm() {
+        return confirm;
+    }
+
+    public void setConfirm(String confirm) {
+        this.confirm = confirm;
+    }
+}
