08e5b68b1686f3b4553be2da7d9544a4b0197ff9
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-12-09 14:38:57 +0530

    initial

---
+++ src/main/java/com/nest/complaintportal_backend/ComplaintportalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.complaintportal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintportalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintportalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/complaintportal_backend/ComplaintportalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.complaintportal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintportalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
